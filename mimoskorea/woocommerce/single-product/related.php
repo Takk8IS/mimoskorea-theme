19bbd4c7b4ef86e0770f7b5be2299e63e46a1f2f
Takk™ Innovate Studio <say@takk.ag>
2026-01-12 13:59:23 +0100

    up

--- mimoskorea/woocommerce/single-product/related.php
+++ mimoskorea/woocommerce/single-product/related.php
@@ -1,61 +1,79 @@
 <?php
+/**
+ * Related Products
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/related.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see         https://woocommerce.com/document/template-structure/
+ * @package     WooCommerce\Templates
+ * @version     10.3.0
+ */
 
-defined('ABSPATH') || exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 global $product;
 
-if (! is_a($product, WC_Product::class)) {
-	return;
+if ( ! isset( $related_products ) || ! is_array( $related_products ) ) {
+	$related_products = [];
 }
 
-$related_ids = wc_get_related_products($product->get_id(), 8, $product->get_upsell_ids());
-$related_products = array();
+if ( empty( $related_products ) && is_a( $product, WC_Product::class ) ) {
+	$related_ids = wc_get_related_products( $product->get_id(), 8, $product->get_upsell_ids() );
+	foreach ( $related_ids as $related_id ) {
+		$related_product = wc_get_product( $related_id );
+		if ( is_a( $related_product, WC_Product::class ) ) {
+			$related_products[] = $related_product;
+		}
+	}
+}
 
-foreach ($related_ids as $related_id) {
-	$related_product = wc_get_product($related_id);
-
-	if (! is_a($related_product, WC_Product::class)) {
-		continue;
+if ( $related_products ) :
+	if ( function_exists( 'wp_increase_content_media_count' ) ) {
+		$content_media_count = wp_increase_content_media_count( 0 );
+		if ( $content_media_count < wp_omit_loading_attr_threshold() ) {
+			wp_increase_content_media_count( wp_omit_loading_attr_threshold() - $content_media_count );
+		}
 	}
 
-	$related_products[] = $related_product;
-}
+	$heading = apply_filters( 'woocommerce_product_related_products_heading', __( 'Related products', 'woocommerce' ) );
 
-if (empty($related_products)) {
-	return;
-}
+	wc_set_loop_prop( 'name', 'related' );
+	wc_set_loop_prop( 'columns', 4 );
+	?>
 
-$heading = apply_filters('woocommerce_product_related_products_heading', __('Related products', 'woocommerce'));
+	<section class="related products">
+		<?php if ( $heading ) : ?>
+			<h2><?php echo esc_html( $heading ); ?></h2>
+		<?php endif; ?>
 
-wc_set_loop_prop('name', 'related');
-wc_set_loop_prop('columns', 4);
+		<div class="related-products-grid">
+			<?php foreach ( $related_products as $related_product ) : ?>
+				<?php
+				if ( ! is_a( $related_product, WC_Product::class ) ) {
+					continue;
+				}
 
-?>
+				$post_object = get_post( $related_product->get_id() );
+				if ( ! $post_object ) {
+					continue;
+				}
 
-<section class="related products">
-	<?php if ($heading) : ?>
-		<h2><?php echo esc_html($heading); ?></h2>
-	<?php endif; ?>
+				setup_postdata( $GLOBALS['post'] = $post_object );
+				wc_get_template_part( 'content', 'product' );
+				?>
+			<?php endforeach; ?>
+		</div>
 
-	<div class="related-products-grid">
-		<?php foreach ($related_products as $related_product) : ?>
-			<?php
-			if (! is_a($related_product, WC_Product::class)) {
-				continue;
-			}
+	</section>
+	<?php
+endif;
 
-			$post_object = get_post($related_product->get_id());
-
-			if (! $post_object) {
-				continue;
-			}
-
-			setup_postdata($GLOBALS['post'] = $post_object);
-
-			wc_get_template_part('content', 'product');
-			?>
-		<?php endforeach; ?>
-	</div>
-</section>
-
-<?php wp_reset_postdata(); ?>
+wp_reset_postdata();
